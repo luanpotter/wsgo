9fde2e4f17a1f02e370cbabf033157e3c7ff49cb
feroult <feroult@gmail.com>
2017-07-31 20:26:16 -0300

    folder org

---
+++ api/src/test/java/wsgo/utils/EndpointTestCase.java
@@ -0,0 +1,12 @@
+package wsgo.utils;
+
+import io.yawp.testing.EndpointTestCaseBase;
+
+public class EndpointTestCase extends EndpointTestCaseBase {
+
+    @Override
+    protected String getAppPackage() {
+        return "wsgo";
+    }
+
+}
