b010f391eab64e863f8332831173659061628d1b
Luan Nico <luanpotter27@gmail.com>
2017-08-06 00:10:12 -0300

    api security

--- api/src/main/java/wsgo/models/settings/SettingsShield.java
+++ api/src/main/java/wsgo/models/settings/SettingsShield.java
@@ -1,13 +1,23 @@
 package wsgo.models.settings;
 
 import io.yawp.repository.shields.Shield;
+import io.yawp.repository.IdRef;
 
 public class SettingsShield extends Shield<Settings> {
 
     @Override
     public void defaults() {
-        // TODO Auto-generated method stub
-        allow();
+        allow(false);
+    }
+
+    @Override
+    public void show(IdRef<Settings> id) {
+        allow(true);
+    }
+
+    @Override
+    public void index(IdRef<?> parentId) {
+        allow(true);
     }
 
 }
--- api/src/test/java/wsgo/models/settings/SettingsTest.java
+++ api/src/test/java/wsgo/models/settings/SettingsTest.java
@@ -3,17 +3,14 @@ package wsgo.models.settings;
 import org.junit.Test;
 import wsgo.utils.EndpointTestCase;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 public class SettingsTest extends EndpointTestCase {
 
     @Test
-    public void testCreate() {
-        // TODO Auto-generated method stub
-        String json = post("/settings", "{}");
-        Settings settings = from(json, Settings.class);
-
-        assertNotNull(settings);
+    public void testList() {
+        String json = get("/settings");
+        assertEquals("[]", json);
     }
 
 }
