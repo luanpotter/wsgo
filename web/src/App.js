bde712a1669358fa76fc3f96ee8cbf74ea00ab00
feroult <feroult@gmail.com>
2017-12-06 17:25:54 -0200

    webapp

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import React, {Component} from 'react'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    return <div className="App">
+      <div className="App-heading App-flex">
+        <h2>Welcome to <span className="App-react">React</span></h2>
       </div>
-    );
+      <div className="App-instructions App-flex">
+        <img className="App-logo" src={require('./react.svg')}/>
+        <p>Edit <code>src/App.js</code> and save to hot reload your changes.</p>
+      </div>
+    </div>
   }
 }
 
-export default App;
+export default App
